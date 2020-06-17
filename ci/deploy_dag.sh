d454137defded13ceb87e8f3288614184ab8e46c
Saurabh Kadam <saurabhkdm721@gmail.com>
2020-06-17 12:19:46 +0530

    Update deploy_dag.sh

--- ci/deploy_dag.sh
+++ ci/deploy_dag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcloud config set project 'qwiklabs-gcp-00-e3f9d53817da'
+
 gcloud composer environments storage dags import \
     --environment highcpu \
     --location us-central1 \
