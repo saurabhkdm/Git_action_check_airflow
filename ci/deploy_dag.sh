54868eac31007cfd66b2cbdaeb8e60ca7019f662
Saurabh Kadam <saurabhkdm721@gmail.com>
2020-06-17 10:50:37 +0530

    Update deploy_dag.sh

--- ci/deploy_dag.sh
+++ ci/deploy_dag.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-set -eux
-
-gcloud init
-
-gcloud auth activate-service-account --key-file $secrets.GOOGLE_APPLICATION_CREDENTIALS
-
-gsutil ls -r  gs://automationbucket2/
-
-
-
+echo "Hello"
